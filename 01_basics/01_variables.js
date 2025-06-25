d5b30d9a0761021898e123940d10306fef269318
shaswats13 <83541226+shaswats13@users.noreply.github.com>
2025-06-25 11:59:56 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "sdwdw@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+/*
+Use let while creating a variable
+ */
+
+accountEmail = "ss@gmail.com"
+accountPassword = "54321"
+accountCity = "Delhi"
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+ "use strict";
+
+   console.log(typeof undefined); //Undefined
+   console.log(typeof null); // Object
+
+   
+   
+   
